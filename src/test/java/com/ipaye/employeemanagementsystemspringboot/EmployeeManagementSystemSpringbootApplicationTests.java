4c8a97ec5835015ce99b1aec21baef5d9c67df77
Ipaye Babatunde <fineboyhakim@gmail.com>
2024-12-11 01:59:41 +0100

    init commit

---
+++ src/test/java/com/ipaye/employeemanagementsystemspringboot/EmployeeManagementSystemSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipaye.employeemanagementsystemspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
