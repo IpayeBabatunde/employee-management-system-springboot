187f1125ee9eb9ca76f3cd8b567f8a8d57fed754
Ipaye Babatunde <fineboyhakim@gmail.com>
2024-12-11 03:09:15 +0100

    init commit

--- src/main/java/com/ipaye/employeemanagementsystemspringboot/controller/EmployeeController.java
+++ src/main/java/com/ipaye/employeemanagementsystemspringboot/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -37,4 +38,15 @@ public class EmployeeController {
     }
 
     @GetMapping("updateform/{id}")
+    public String updateform(@PathVariable(value= "id") long id, Model model){
+        Employee employee = employeeService.getById(id);
+        model.addAttribute("employee", employee);
+        return "update";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteById(@PathVariable(value="id") long id){
+        employeeService.deleteById(id);
+        return "redirect:/";
+    }
 }
